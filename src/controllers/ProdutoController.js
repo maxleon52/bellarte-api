581bbf2b8d21212e7102e5371859a15a15bc84ad
maxleon52 <maxleon522@gmail.com>
2020-03-17 22:38:21 -0300

    Fix pesquisa um produto

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -10,7 +10,7 @@ module.exports = {
 
   //Busca UM
   async show(req, res) {
-    const { name } = req.body;
+    const { name } = req.query;
     const response = await Produto.find({ name: new RegExp(name, "i") });
 
     return res.json(response);
